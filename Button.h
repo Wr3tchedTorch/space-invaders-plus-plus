c9f5c9d36fe37a7cb5e012c83583fab50429259c
Wr3tchedTorch <ericmoura511@gmail.com>
2026-02-24 17:04:59 -0300

    add Button class definition

--- Button.h
+++ Button.h
@@ -1,5 +1,24 @@
 #pragma once
+#include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Rect.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <string>
+
 class Button
 {
+private:
+	sf::RectangleShape m_Background;
+	sf::Font m_Font;
+
+public:
+	sf::Text m_Text;
+	sf::FloatRect m_Collider;
+
+	Button(sf::FloatRect bounds, sf::Color backgroundColor, std::string text);
+
+	void draw(sf::RenderWindow& window);
 };
 
