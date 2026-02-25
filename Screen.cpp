8614db7ef5ca8112c7130cbae0e1c6d525cb7be3
Wr3tchedTorch <ericmoura511@gmail.com>
2026-02-25 11:20:40 -0300

    add definition and implementation of Screen class

---
+++ Screen.cpp
@@ -0,0 +1,44 @@
+#include "Screen.h"
+#include <memory>
+#include <utility>
+#include <SFML/Graphics/RenderStates.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include "InputHandler.h"
+#include "ScreenManagerRemoteControl.h"
+#include "UIPanel.h"
+
+void Screen::addPanel(std::unique_ptr<UIPanel> panel, std::shared_ptr<InputHandler> inputHandler, ScreenManagerRemoteControl* screenManagerRemoteControl)
+{
+	inputHandler->initializeHandler(this, &panel->m_View, screenManagerRemoteControl, panel->getButtons());
+
+	m_UIPanels.push_back(std::move(panel));
+	m_InputHandlers.push_back(inputHandler);
+}
+
+void Screen::initialize()
+{
+}
+
+void Screen::update(float delta)
+{
+}
+
+void Screen::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	for (auto& panel : m_UIPanels)
+	{
+		target.draw(*panel);
+	}
+}
+
+void Screen::handleInput(sf::RenderWindow& window)
+{
+	while (auto event = window.pollEvent())
+	{
+		for (auto& inputHandler : m_InputHandlers)
+		{
+			inputHandler->handleInput(*event, window);
+		}
+	}
+}
---
+++ Screen.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <vector>
+#include <memory>
+#include <SFML/Graphics/Drawable.hpp>
+#include <SFML/Graphics/RenderStates.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Graphics/View.hpp>
+#include "InputHandler.h"
+#include "UIPanel.h"
+#include "ScreenManagerRemoteControl.h"
+
+class Screen : public sf::Drawable
+{
+private:
+	std::vector<std::shared_ptr<InputHandler>> m_InputHandlers;
+	std::vector<std::unique_ptr<UIPanel>> m_UIPanels;
+
+protected:
+	void addPanel(
+		std::unique_ptr<UIPanel> panel, 
+		std::shared_ptr<InputHandler> inputHandler, 
+		ScreenManagerRemoteControl* screenManagerRemoteControl
+	);
+
+public:
+	sf::View m_View;
+
+	virtual void initialize();
+	
+	virtual void update(float delta);
+	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+	virtual void handleInput(sf::RenderWindow& window);
+};
+
