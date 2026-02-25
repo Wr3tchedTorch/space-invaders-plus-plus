6f7bbc38823bcc4da964b265b3004d38eaf7585c
Wr3tchedTorch <ericmoura511@gmail.com>
2026-02-25 09:40:10 -0300

    finish adding InputHandler class definition

---
+++ InputHandler.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <SFML/Graphics/View.hpp>
+#include <memory>
+#include <vector>
+#include "ScreenManagerRemoteControl.h"
+#include "Button.h"
+
+class Screen;
+
+class InputHandler
+{
+private:
+	Screen*	  m_ParentScreen;
+	sf::View* m_PointerToUIPanelView;
+	ScreenManagerRemoteControl* m_ScreenManagerRemoteControl;
+
+	std::vector<std::shared_ptr<Button>> m_Buttons;
+
+public:
+	void initializeHandler(
+		Screen*	  parentScreen,
+		sf::View* pointerToUIView,
+		ScreenManagerRemoteControl* screenManagerRemoteControl,
+		std::vector<std::shared_ptr<Button>> buttons
+	);
+
+	void handleInput(sf::Event& event, sf::RenderWindow& window);
+	virtual void handleGamepad();
+	virtual void handleKeyPressed(sf::Event& event, sf::RenderWindow& window);
+	virtual void handleKeyReleased(sf::Event& event, sf::RenderWindow& window);
+	virtual void handleLeftMouseClick(std::string& buttonInteractedWith, sf::RenderWindow& window);
+
+	sf::View* getPointerToView();
+	Screen* getPointerToParentScreen();
+
+	ScreenManagerRemoteControl* getPointerToScreenManagerRemoteControl();
+};
+
