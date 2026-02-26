90f9770286345a1d1cba3b644da31b474e20bed1
Wr3tchedTorch <ericmoura511@gmail.com>
2026-02-26 10:50:31 -0300

    add initial definition and implementation of GameScreen class

---
+++ GameScreen.cpp
@@ -0,0 +1,75 @@
+#include "GameScreen.h"
+#include "WorldState.h"
+
+GameScreen::GameScreen(sf::Vector2u resolution, ScreenManagerRemoteControl* screenManagerRemoteControl, sf::Texture& backgroundTexture) :
+	m_BackgroundTexture(backgroundTexture),
+	m_BackgroundSprite(backgroundTexture)
+{
+	m_ScreenManagerRemoteControl = screenManagerRemoteControl;	
+
+	m_GameInputHandler = std::make_shared<GameInputHandler>();
+	auto gameUIPanel   = std::make_unique<GameUIPanel>();
+	addPanel(gameUIPanel, m_GameInputHandler, m_ScreenManagerRemoteControl);
+
+	auto gameOverInputHandler = std::make_shared<GameOverInputHandler>();
+	auto gameOverUIPanel      = std::make_unique<GameOverUIPanel>();
+	addPanel(gameOverUIPanel, gameOverInputHandler, m_ScreenManagerRemoteControl);
+
+	float ratio = static_cast<float>(resolution.x) / resolution.y;
+	
+	WorldState::WORLD_HEIGHT = WorldState::WORLD_WIDTH / ratio;
+
+	m_View.setSize({   WorldState::WORLD_WIDTH,   WorldState::WORLD_HEIGHT });
+	m_View.setCenter({ WorldState::WORLD_WIDTH/2, WorldState::WORLD_HEIGHT/2 });
+
+	m_BackgroundSprite.setScale(
+		{
+			m_View.getSize().x / m_BackgroundTexture.getSize().x,
+			m_View.getSize().y / m_BackgroundTexture.getSize().y
+		});
+}
+
+void GameScreen::initialize()
+{
+	m_GameInputHandler->initialize();
+
+	WorldState::NUM_INVADERS = 0;
+
+	if (WorldState::WAVE_NUMBER == 0)
+	{
+		WorldState::NUM_INVADERS_AT_START = WorldState::NUM_INVADERS;
+		WorldState::WAVE_NUMBER = 1;
+		WorldState::LIVES = 3;
+		WorldState::SCORE = 0;
+	}
+}
+
+void GameScreen::update(float delta)
+{
+	Screen::update(delta);
+
+	if (m_GameOver)
+	{
+		return;
+	}
+
+	if (WorldState::LIVES <= 0)
+	{
+		m_GameOver = true;
+		return;
+	}
+
+	if (WorldState::NUM_INVADERS <= 0)
+	{
+		WorldState::WAVE_NUMBER++;
+		m_ScreenManagerRemoteControl->loadLevelInPlayMode("level1");
+	}
+}
+
+void GameScreen::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+	target.setView(m_View);
+	target.draw(m_BackgroundSprite);
+
+	Screen::draw(target, states);
+}
---
+++ GameScreen.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <memory>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Texture.hpp>
+#include <SFML/System/Vector2.hpp>
+#include "Screen.h"
+#include "ScreenManagerRemoteControl.h"
+#include <SFML/Graphics/RenderStates.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+
+class GameScreen : public Screen
+{
+private:
+	ScreenManagerRemoteControl* m_ScreenManagerRemoteControl;
+	std::shared_ptr<GameInputHandler> m_GameInputHandler;
+
+	sf::Texture m_BackgroundTexture;
+	sf::Sprite  m_BackgroundSprite;
+
+public:
+	static bool m_GameOver;
+
+	GameScreen(sf::Vector2u resolution, ScreenManagerRemoteControl* screenManagerRemoteControl, sf::Texture& backgroundTexture);
+	
+	void initialize() override;
+	void update(float delta) override;
+	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+};
+
