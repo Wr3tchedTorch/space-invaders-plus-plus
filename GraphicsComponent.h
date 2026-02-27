6ac660d005382b0fda33a8806d9d70f94df983f6
Wr3tchedTorch <ericmoura511@gmail.com>
2026-02-27 09:52:43 -0300

    added a new pure function for initializing graphics on GraphicsComponent

--- GraphicsComponent.h
+++ GraphicsComponent.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "Component.h"
 #include <SFML/Graphics/RenderTarget.hpp>
+#include <string>
+#include <SFML/System/Vector2.hpp>
 #include "TransformComponent.g"
 
 class GraphicsComponent : Component
@@ -10,6 +12,7 @@ class GraphicsComponent : Component
 
 public:
 	virtual void draw(sf::RenderTarget& target, std::shared_ptr<TransformComponent> transformComponent) = 0;
+	virtual void initializeGraphics(std::string bitmapName, sf::Vector2f objectSize) = 0;
 
 	std::string getType() override;
 	std::string getSpecificType() override;
