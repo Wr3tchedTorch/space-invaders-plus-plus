1dae9f54b85be5f4135a1f3566d4ccc82cca33af
Wr3tchedTorch <ericmoura511@gmail.com>
2026-03-03 17:15:42 -0300

    implement detectInvaderCollisions function

--- PhysicsEnginePlayMode.cpp
+++ PhysicsEnginePlayMode.cpp
@@ -1,4 +1,48 @@
 #include "PhysicsEnginePlayMode.h"
+#include <vector>
+#include "GameObject.h"
+#include "GameObjectSharer.h"
+#include "BulletUpdateComponent.h"
+#include <memory>
+#include "SoundEngine.h"
+#include <SFML/System/Vector2.hpp>
+#include "WorldState.h"
+
+void PhysicsEnginePlayMode::detectInvaderCollisions(std::vector<GameObject>& objects, const std::vector<int>& bulletPositions)
+{
+	sf::Vector2f offScreen({ -1, -1 });
+
+	for (auto& invader : objects)
+	{
+		if (invader.getTag() != "invader" || !invader.isActive())
+		{
+			continue;
+		}
+		for (auto& bulletIndex : bulletPositions)
+		{
+			auto& bullet = objects.at(bulletIndex);
+			if (bullet.getTag() != "bullet" || !std::static_pointer_cast<BulletUpdateComponent>(bullet.getFirstUpdateComponent())->m_BelongsToPlayer)
+			{
+				continue;
+			}
+
+			bool colided = bullet.getEncompassingRectCollider().findIntersection(invader.getEncompassingRectCollider()).has_value();
+			if (!colided)
+			{
+				continue;
+			}
+
+			SoundEngine::playInvaderExplosion();
+			invader.getTransformComponent()->getLocation() = offScreen;
+			bullet.getTransformComponent()->getLocation()  = offScreen;
+
+			WorldState::SCORE++;
+			WorldState::NUM_INVADERS--;
+
+			invader.setInactive();
+		}
+	}
+}
 
 void PhysicsEnginePlayMode::initialize(GameObjectSharer& gameObjectSharer)
 {
